b0b835b0ff43af7150fa4241255894fb90e81169
Venkatesan Chandra Sekaran <venkatesan_dinesh@icloud.com>
2025-05-27 17:32:29 +0530

    Bills name Started

--- src/features/doctor/pages/Bills/Bills.tsx
+++ src/features/doctor/pages/Bills/Bills.tsx
@@ -7,7 +7,7 @@ const mockPatients = [
   { id: 'P001', name: 'John Doe' },
   { id: 'P002', name: 'Emma Lee' },
   { id: 'P003', name: 'Alex Roy' },
-   { id: 'P004', name: 'Alex Roy' },
+   { id: 'P004', name: 'Dinesh' },
 ];
 
 const patientBills = {
